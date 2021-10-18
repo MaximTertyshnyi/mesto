af1f305c48a3842b95f6999154c05c2970c794d4
MaximTertyshnyi <89603158+MaximTertyshnyi@users.noreply.github.com>
2021-10-19 00:12:58 +0300

    Add files via upload

---
+++ scripts/index.js
@@ -0,0 +1,54 @@
+// Делаем выборку DOM элементов
+const popupOpenButtonElement = document.querySelector('.edit-button')
+const popupElement = document.querySelector('.popup')
+const popupCloseButtonElement = popupElement.querySelector('.popup__close')
+
+const togglePopupVisibility = function () {
+    popupElement.classList.toggle('popup_opened')
+}
+
+const openPopup = function () {
+    popupElement.classList.add('popup_opened')
+}
+
+const closePopup = function () {
+    popupElement.classList.remove('popup_opened')
+}
+
+const closePopupByClickOnOverlay = function (event) {
+    console.log(event.target, event.currentTarget)
+    if (event.target !== event.currentTarget) {
+        return
+    }
+
+    closePopup()
+}
+
+popupOpenButtonElement.addEventListener('click', openPopup)
+popupCloseButtonElement.addEventListener('click', closePopup)
+popupElement.addEventListener('click', closePopupByClickOnOverlay)
+
+let nameElement = document.querySelector(".profile__title")
+let descriptionElement = document.querySelector(".profile__subtitle")
+
+//Редактирование имени и информации о себе
+// Находим форму в DOM
+let formElement = document.querySelector(".popup__form")
+// Находим поля формы в DOM
+let nameInput = formElement.querySelector(".popup__text-name")
+let jobInput = formElement.querySelector(".popup__text-business")
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+    // Получите значение полей jobInput и nameInput из свойства value
+    let nameInputValue = nameInput.value;
+    let jobInputValue = jobInput.value;
+    // Выберите элементы, куда должны быть вставлены значения полей
+    nameElement.textContent = nameInputValue;
+    descriptionElement.textContent = jobInputValue;
+    closePopup();
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
